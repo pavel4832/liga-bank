e7f0126837629db59c192e02c7d1bc1e8f6c37fd
pavel <paveluv@mail.ru>
2021-05-04 01:01:52 +0300

    3. add webpack and babel

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMod>
     <App />
-  </React.StrictMode>,
+  </React.StrictMod>,
   document.getElementById('root')
 );
 
